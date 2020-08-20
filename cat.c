45bf66b9f1d7eabfd9929d2aeeb4fd4dd261f25b
qwertybomb <42456119+qwertybomb@users.noreply.github.com>
2020-08-20 08:14:04 -0400

    Add files via upload

--- cat.c
+++ cat.c
@@ -9,189 +9,70 @@ char *output_buffer = NULL;
 DWORD output_capacity = 0;
 
 /* There is only CommandLineToArgvW so a version for ascii is needed */
-static LPSTR *CommandLineToArgvA(_In_opt_ LPCSTR lpCmdLine, _Out_ int *pNumArgs)
+LPSTR *CommandLineToArgvA(LPSTR lpCmdLine, INT *pNumArgs)
 {
-	DWORD argc;
-	LPSTR *argv;
-	LPSTR s;
-	LPSTR d;
-	LPSTR cmdline;
-	int qcount, bcount;
+	int retval;
+	retval = MultiByteToWideChar(CP_ACP, MB_ERR_INVALID_CHARS, lpCmdLine, -1, NULL, 0);
+	if (!SUCCEEDED(retval))
+		return NULL;
 
-	if (!pNumArgs || *lpCmdLine == 0) {
-		SetLastError(ERROR_INVALID_PARAMETER);
+	LPWSTR lpWideCharStr = HeapAlloc(GetProcessHeap(), 0, retval * sizeof(WCHAR));
+	if (lpWideCharStr == NULL)
+		return NULL;
+
+	retval = MultiByteToWideChar(CP_ACP, MB_ERR_INVALID_CHARS, lpCmdLine, -1, lpWideCharStr, retval);
+	if (!SUCCEEDED(retval)) {
+		HeapFree(GetProcessHeap(), 0, lpWideCharStr);
 		return NULL;
 	}
 
-	/* --- First count the arguments */
-	argc = 1;
-	s = lpCmdLine;
-	/* The first argument, the executable path, follows special rules */
-	if (*s == '"') {
-		/* The executable path ends at the next quote, no matter what */
-		s++;
-		while (*s)
-			if (*s++ == '"')
-				break;
-	}
-	else {
-		/* The executable path ends at the next space, no matter what */
-		while (*s && *s != ' ' && *s != '\t')
-			s++;
-	}
-	/* skip to the first argument, if any */
-	while (*s == ' ' || *s == '\t')
-		s++;
-	if (*s)
-		argc++;
-
-	/* Analyze the remaining arguments */
-	qcount = bcount = 0;
-	while (*s) {
-		if ((*s == ' ' || *s == '\t') && qcount == 0) {
-			/* skip to the next argument and count it if any */
-			while (*s == ' ' || *s == '\t')
-				s++;
-			if (*s)
-				argc++;
-			bcount = 0;
-		}
-		else if (*s == '\\') {
-			/* '\', count them */
-			bcount++;
-			s++;
-		}
-		else if (*s == '"') {
-			/* '"' */
-			if ((bcount & 1) == 0)
-				qcount++; /* unescaped '"' */
-			s++;
-			bcount = 0;
-			/* consecutive quotes, see comment in copying code below */
-			while (*s == '"') {
-				qcount++;
-				s++;
-			}
-			qcount = qcount % 3;
-			if (qcount == 2)
-				qcount = 0;
-		}
-		else {
-			/* a regular character */
-			bcount = 0;
-			s++;
-		}
-	}
-
-	/* Allocate in a single lump, the string array, and the strings that go
-	 * with it. This way the caller can make a single LocalFree() call to free
-	 * both, as per MSDN.
-	 */
-
-	argv = LocalAlloc(LMEM_FIXED, (argc + 1) * sizeof(LPSTR) + (lstrlenA(lpCmdLine) + 1) * sizeof(char));
-	if (!argv)
+	int numArgs;
+	LPWSTR *args;
+	args = CommandLineToArgvW(lpWideCharStr, &numArgs);
+	HeapFree(GetProcessHeap(), 0, lpWideCharStr);
+	if (args == NULL)
 		return NULL;
-	cmdline = (LPSTR)(argv + argc + 1);
-	lstrcpyA(cmdline, lpCmdLine);
 
-	/* --- Then split and copy the arguments */
-	argv[0] = d = cmdline;
-	argc = 1;
-	/* The first argument, the executable path, follows special rules */
-	if (*d == '"') {
-		/* The executable path ends at the next quote, no matter what */
-		s = d + 1;
-		while (*s) {
-			if (*s == '"') {
-				s++;
-				break;
-			}
-			*d++ = *s++;
+	int storage = numArgs * sizeof(LPSTR);
+	for (int i = 0; i < numArgs; ++i) {
+		BOOL lpUsedDefaultChar = FALSE;
+		retval = WideCharToMultiByte(CP_ACP, 0, args[i], -1, NULL, 0, NULL, &lpUsedDefaultChar);
+		if (!SUCCEEDED(retval)) {
+			LocalFree(args);
+			return NULL;
 		}
-	}
-	else {
-		/* The executable path ends at the next space, no matter what */
-		while (*d && *d != ' ' && *d != '\t')
-			d++;
-		s = d;
-		if (*s)
-			s++;
-	}
-	/* close the executable path */
-	*d++ = 0;
-	/* skip to the first argument and initialize it if any */
-	while (*s == ' ' || *s == '\t')
-		s++;
-	if (!*s) {
-		/* There are no parameters so we are all done */
-		argv[argc] = NULL;
-		*pNumArgs = argc;
-		return argv;
+
+		storage += retval;
 	}
 
-	/* Split and copy the remaining arguments */
-	argv[argc++] = d;
-	qcount = bcount = 0;
-	while (*s) {
-		if ((*s == ' ' || *s == '\t') && qcount == 0) {
-			/* close the argument */
-			*d++ = 0;
-			bcount = 0;
-
-			/* skip to the next one and initialize it if any */
-			do {
-				s++;
-			} while (*s == ' ' || *s == '\t');
-			if (*s)
-				argv[argc++] = d;
-		}
-		else if (*s == '\\') {
-			*d++ = *s++;
-			bcount++;
-		}
-		else if (*s == '"') {
-			if ((bcount & 1) == 0) {
-				/* Preceded by an even number of '\', this is half that
-				 * number of '\', plus a quote which we erase.
-				 */
-				d -= bcount / 2;
-				qcount++;
-			}
-			else {
-				/* Preceded by an odd number of '\', this is half that
-				 * number of '\' followed by a '"'
-				 */
-				d = d - bcount / 2 - 1;
-				*d++ = '"';
-			}
-			s++;
-			bcount = 0;
-			/* Now count the number of consecutive quotes. Note that qcount
-			 * already takes into account the opening quote if any, as well as
-			 * the quote that lead us here.
-			 */
-			while (*s == '"') {
-				if (++qcount == 3) {
-					*d++ = '"';
-					qcount = 0;
-				}
-				s++;
-			}
-			if (qcount == 2)
-				qcount = 0;
-		}
-		else {
-			/* a regular character */
-			*d++ = *s++;
-			bcount = 0;
-		}
+	LPSTR *result = (LPSTR *)LocalAlloc(LMEM_FIXED, storage);
+	if (result == NULL) {
+		LocalFree(args);
+		return NULL;
 	}
-	*d = '\0';
-	argv[argc] = NULL;
-	*pNumArgs = argc;
 
-	return argv;
+	int bufLen = storage - numArgs * sizeof(LPSTR);
+	LPSTR buffer = ((LPSTR)result) + numArgs * sizeof(LPSTR);
+	for (int i = 0; i < numArgs; ++i) {
+		BOOL lpUsedDefaultChar = FALSE;
+		retval = WideCharToMultiByte(CP_ACP, 0, args[i], -1, buffer, bufLen, NULL, &lpUsedDefaultChar);
+		if (!SUCCEEDED(retval)) {
+			LocalFree(result);
+			LocalFree(args);
+			return NULL;
+		}
+
+		result[i] = buffer;
+		buffer += retval;
+		bufLen -= retval;
+	}
+
+	LocalFree(args);
+
+	*pNumArgs = numArgs;
+	return result;
 }
+
 
 static void lmemcpy(char *dest, const char *src, DWORD len)
 {
